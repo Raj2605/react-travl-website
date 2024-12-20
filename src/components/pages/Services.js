a992962f3093bd31c35d3033dba52549383da797
Raj2605 <117986686+Raj2605@users.noreply.github.com>
2024-12-20 14:25:52 +0530

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -3,4 +3,9 @@ import'../../App.css';
 
 export default function Services() {
     return <h1 className="services">SERVICES</h1>;
+    <button type="button" class="btn btn-primary">Services 1</button>
+<button type="button" class="btn btn-secondary">Services 2</button>
+<button type="button" class="btn btn-success">Services 3</button>
+<button type="button" class="btn btn-danger">Services 4</button>
+    
 }
